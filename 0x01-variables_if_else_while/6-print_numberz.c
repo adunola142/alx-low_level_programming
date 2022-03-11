f00d7dd0b5ac6733d6a822fb75198538474f8849
Tura <adunola142@gmail.com>
2022-03-11 05:01:55 -0800

    correction : 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 /**
- *
  * main - Print all single digit numbers using only putchar
  *
  * Return: Always 0 (Success)
@@ -10,6 +9,7 @@
 int main(void)
 {
 	int digit;
+
 	for (digit = 0; digit < 10; digit++)
 	putchar(digit + '0');
 
