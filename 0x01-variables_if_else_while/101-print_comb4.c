885a660743212fea225d1271c190f46a49c32d9e
Tura <adunola142@gmail.com>
2022-03-14 00:21:07 -0700

    correction : 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,11 +11,11 @@ int main(void)
 {
 	int digit1, digit2, digit3;
 
-	for (digit1 = 48; digit1 < 56; digit1++)
+	for (digit1 = 0; digit1 < 9; digit1++)
 	{
-		for (digit2 = digit1 + 1; digit2 < 57; digit2++)
+		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
 		{
-			for (digit3 = digit2 + 1; digit3 < 58; digit3++)
+			for (digit3 = digit2 + 1; digit3 < 9; digit3++)
 			{
 				putchar(digit1);
 				putchar(digit2);
