cc77e2fc469c35fa5046f241cd19c33b6a45ebaf
Tura <adunola142@gmail.com>
2022-03-17 04:32:54 -0700

    fixed 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,10 +5,17 @@
  *
  * Return: The last digit
  */
-int print_last_digit(int n)
+int print_last_digit(int id)
 {
-	if (n < 0)
-		n = n * -1;
-	_putchar((n % 10) + '0');
-	return (n % 10);
+	if (id > 0)
+	{
+		id = id % 10;
+		_putchar(id + '0');
+	}
+	else
+	{
+		id = id % 10 * -1;
+		_putchar(id + '0');
+	}
+	return (id);
 }
