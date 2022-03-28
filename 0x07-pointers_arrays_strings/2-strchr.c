fa513c4abed7cc78a5a6ad31bed53d29b81f63ee
Tura <54582407+adunola142@users.noreply.github.com>
2022-03-28 20:59:02 +0100

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,7 +4,8 @@
  * @s: the string
  * @c: the character
  *
- * Return: a pointer to a character aka the character found
+ * Return: a pointer to a character aka 
+ * the character found
  */
 char *_strchr(char *s, char c)
 {
